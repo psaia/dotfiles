5d5d144ccf940376e67a7853046cee884adc567c
Pete Saia <pete.saia@nytimes.com>
2023-11-08 14:13:07 -0500

    Add go formatting

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -176,6 +176,18 @@ return {
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
     opts = {},
+    config = function ()
+      require("go").setup()
+
+      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+         require('go.format').goimport()
+        end,
+        group = format_sync_grp,
+      })
+    end
   },
   {
     "cuducos/yaml.nvim",
