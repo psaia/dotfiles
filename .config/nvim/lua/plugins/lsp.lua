9d76c3c695c297e3dde4e15a01519ff9baf337e3
Pete Saia <iam@petesaia.com>
2024-12-19 11:25:58 -0500

    Remove lsp zero and upgrade

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,34 +1,14 @@
 return {
   {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    lazy = true,
-    config = function()
-      -- This is where you modify the settings for lsp-zero
-      -- Note: autocompletion settings will not take effect
-
-      require("lsp-zero.settings").preset({})
-    end,
-  },
-
-  -- Autocompletion
-  {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
       { "L3MON4D3/LuaSnip" },
     },
     config = function()
-      -- Here is where you configure the autocompletion settings.
-      -- The arguments for .extend() have the same shape as `manage_nvim_cmp`:
-      -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/api-reference.md#manage_nvim_cmp
-
-      require("lsp-zero.cmp").extend()
-
       -- And you can configure cmp even more, if you want to.
       local cmp = require("cmp")
       local luasnip = require("luasnip")
-      local cmp_action = require("lsp-zero.cmp").action()
 
       cmp.setup({
         snippet = {
@@ -71,15 +51,39 @@ return {
       { "hrsh7th/cmp-nvim-lsp", "lukas-reineke/lsp-format.nvim" },
     },
     config = function()
-      local lsp = require("lsp-zero")
-
-      lsp.on_attach(function(client, bufnr)
-        lsp.default_keymaps({buffer = bufnr})
-        --lsp.buffer_autoformat()
-      end)
-
       -- Configure all servers here.
       local lspconfig = require('lspconfig')
+
+      -- Add cmp_nvim_lsp capabilities settings to lspconfig
+      -- This should be executed before you configure any language server
+      local lspconfig_defaults = lspconfig.util.default_config
+      lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+        'force',
+        lspconfig_defaults.capabilities,
+        require('cmp_nvim_lsp').default_capabilities()
+      )
+
+      -- This is where you enable features that only work
+      -- if there is a language server active in the file
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP actions',
+        callback = function(event)
+          local opts = {buffer = event.buf}
+
+          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+          vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+          vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+          vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+          vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+          vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+          vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+          vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+          vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        end,
+      })
+
+      -- Configure language servers.
       lspconfig.gopls.setup({
         settings = {
           gopls = {
@@ -151,8 +155,6 @@ return {
           documentRangeFormatting = true,
         },
       })
-
-      lsp.setup()
     end,
   },
   "ChiliConSql/neovim-stylus",
@@ -207,7 +209,6 @@ return {
   },
   {
     'creativenull/efmls-configs-nvim',
-    version = 'v1.x.x', -- version is optional, but recommended
     dependencies = { 'neovim/nvim-lspconfig' },
   },
   {
