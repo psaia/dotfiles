b7c1bd50eefe1c0d0db67166696a0a99b7e8e12f
Pete Saia <iam@petesaia.com>
2025-03-05 16:54:06 -0700

    Fix autocomplete

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -16,6 +16,23 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
         sources = {
           { name = "nvim_lsp" },
           { name = "luasnip" },
